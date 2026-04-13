a05d3725318d8b5ac695c5dcb23628e2ddaeedae
Hikkstar <54229046+Hikkstar@users.noreply.github.com>
2026-04-13 21:43:50 +0900

    Update app.vue

--- app.vue
+++ app.vue
@@ -51,6 +51,7 @@
         <h1 class="display-4">Welcome to Our Responsive Website</h1>
         <p class="lead">Discover amazing things with Bootstrap 5</p>
         <a class="btn btn-primary btn-lg" href="#" role="button">Learn More</a>
+        <img src="@/assets/img/10023.jpg" class="card-img-top" alt="yugioh">
     </div>
 </section>
 
