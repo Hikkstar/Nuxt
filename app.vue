0e0584939470b296b99310f93b40f7f8ed79e21e
Hikkstar <54229046+Hikkstar@users.noreply.github.com>
2026-04-13 22:15:16 +0900

    Increase image size in app.vue

--- app.vue
+++ app.vue
@@ -55,7 +55,7 @@
 </section>
 
     <div class="flex justify-center w-screen">
-        <img src="@/assets/img/10023.jpg" class="w-24 h-24 object-contain">
+        <img src="@/assets/img/10023.jpg" class="w-48 h-48 object-contain">
   </div>
 
 <!-- Service Section -->
