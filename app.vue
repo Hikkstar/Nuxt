1c4c60c0df4a7ed906bf954fb4efe37aa4842e2e
Hikkstar <54229046+Hikkstar@users.noreply.github.com>
2026-04-13 22:34:09 +0900

    Resize image in app.vue from 16x16 to 12x12

--- app.vue
+++ app.vue
@@ -55,7 +55,7 @@
 </section>
 
     <div class="text-center">
-      <img src="@/assets/img/10023.jpg" class="w-16 h-16 object-contain">
+      <img src="@/assets/img/10023.jpg" class="w-12 h-12 object-contain">
     </div>
 
 <!-- Service Section -->
